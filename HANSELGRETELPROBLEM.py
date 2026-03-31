d1e33639d0d5af8c180925d26a9475c015f01160
SIgmaridsp <gongalec@gmail.com>
2026-03-31 20:13:52 +1100

    Fixed RECURRING GRID SiZE ISSUE next to fix is the direction but use integer value

--- HANSELGRETELPROBLEM.py
+++ HANSELGRETELPROBLEM.py
@@ -1,15 +1,17 @@
 table = []
+sizeNotSet = True
 # replace dots with x every tim command given
 while True:
-    size = int(input("Grid size: "))
+    if sizeNotSet == True:
+      size = int(input("Grid size: "))
+    sizeNotSet = False
     row = []
     for j in range(size):
         row.append(".")
     for k in range(size):
         table.append(row)
-
-    direct = input("Direction: ")
     for row in table:
         print("".join(row))
+    direct = input("Direction: ")
     if direct == "":
         break
