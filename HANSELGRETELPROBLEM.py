da80849423793542efbc55b6b013c411ac6ddd3e
SIgmaridsp <gongalec@gmail.com>
2026-03-31 20:40:36 +1100

    The x for Grid Size Working     _Next Up fixing the directions for X movement

--- HANSELGRETELPROBLEM.py
+++ HANSELGRETELPROBLEM.py
@@ -1,17 +1,30 @@
 table = []
 sizeNotSet = True
 # replace dots with x every tim command given
+y = 0
+x = 0
 while True:
     if sizeNotSet == True:
-      size = int(input("Grid size: "))
+        size = int(input("Grid size: "))
     sizeNotSet = False
-    row = []
-    for j in range(size):
-        row.append(".")
     for k in range(size):
+        row = []
+        for j in range(size):
+            row.append(".")
         table.append(row)
+    table[x][y] = "x"
     for row in table:
         print("".join(row))
     direct = input("Direction: ")
     if direct == "":
         break
+    else:
+        if direct == "up":
+            y -= 1
+        elif direct == "down":
+            y += 1
+        elif direct == "left":
+            x -= 1
+        elif direct == "right":
+            x += 1
+        table[y][x] = "x"
